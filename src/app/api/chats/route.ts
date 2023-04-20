a8853afb99c34033275300e32baf456b15ac0211
davidrogger <davidrogger.a@gmail.com>
2023-04-20 21:45:59 +0000

    FEAT: post new chats request

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -3,5 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_request: NextRequest) {
   const chats = await prisma.chat.findMany();
-  return NextResponse.json( chats )
+  return NextResponse.json( chats );
+}
+
+export async function POST(request:NextRequest) {
+  const { message } = await request.json();
+  const chatCreated = await prisma.chat.create({
+    data: {
+      message
+    }
+  });
+
+  return NextResponse.json(chatCreated)
 }
